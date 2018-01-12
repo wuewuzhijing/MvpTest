165c62624553c38521d518b9c62196ae3c06ee59
ly <15820480843@163.com>
2018-01-12 19:40:07 +0800

    添加一些基础配置

---
+++ app/src/main/java/com/example/administrator/mvptest/app/GlobalConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.administrator.mvptest.app;
+
+
+import android.app.Application;
+import android.content.Context;
+import android.support.v4.app.FragmentManager;
+
+import com.jess.arms.base.delegate.AppLifecycles;
+import com.jess.arms.di.module.GlobalConfigModule;
+import com.jess.arms.integration.ConfigModule;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/1/12 0012.
+ */
+
+public class GlobalConfiguration implements ConfigModule {
+    @Override
+    public void applyOptions(Context context, GlobalConfigModule.Builder builder) {
+
+    }
+
+    @Override
+    public void injectAppLifecycle(Context context, List<AppLifecycles> lifecycles) {
+
+    }
+
+    @Override
+    public void injectActivityLifecycle(Context context, List<Application.ActivityLifecycleCallbacks> lifecycles) {
+
+    }
+
+    @Override
+    public void injectFragmentLifecycle(Context context, List<FragmentManager.FragmentLifecycleCallbacks> lifecycles) {
+
+    }
+}
